fbaad6695ddcf777d9914933f5fca093b1adc5c4
zenkle <1481038024@qq.com>
2026-03-17 12:01:51 +0800

    build: 添加 unocss 和 pinia 依赖
    添加 unocss 用于原子化 CSS 支持，并引入 pinia 作为状态管理库

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import path from "path";
+import UnoCSS from "unocss/vite";
+
 const autoImportDts = path.resolve(__dirname, "auto-imports.d.ts");
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
     AutoImport({
       imports: ["vue"],
       eslintrc: {
